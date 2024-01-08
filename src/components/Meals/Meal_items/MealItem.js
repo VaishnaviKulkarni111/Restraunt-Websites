6e42b19b356ae505c70bcb91eaadbbe1ef573f52
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2024-01-08 13:00:22 +0530

    task 4

--- src/components/Meals/Meal_items/MealItem.js
+++ src/components/Meals/Meal_items/MealItem.js
@@ -1,5 +1,5 @@
 import classes from './MealItem.module.css';
-
+import MealForm from './MealForm';
 
 
 const MealIteam = (props) => {
@@ -8,12 +8,12 @@ const MealIteam = (props) => {
     return(
       <li className={classes.meal}>
         <div> 
-        <h3>{props.name}</h3>
+        <h3>{props.name}  </h3>
         <div className={classes.description}>{props.description}</div>
-        <div className={classes.price}>{price}</div>
+        <div className={classes.price}>{price} </div>
         </div>
         <div>
-
+        <MealForm/> 
         </div>
        
       </li>  
